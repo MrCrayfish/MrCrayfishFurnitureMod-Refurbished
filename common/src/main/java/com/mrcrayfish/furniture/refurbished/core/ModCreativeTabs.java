b303eb569a46f148694ed76a85a538a30316da5e
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2023-08-04 19:31:49 +0930

    🐛 Fixed items not showing in creative search tab

--- common/src/main/java/com/mrcrayfish/furniture/refurbished/core/ModCreativeTabs.java
+++ common/src/main/java/com/mrcrayfish/furniture/refurbished/core/ModCreativeTabs.java
@@ -1,14 +1,17 @@
 package com.mrcrayfish.furniture.refurbished.core;
 
+import com.mrcrayfish.framework.Registration;
 import com.mrcrayfish.framework.api.registry.RegistryContainer;
 import com.mrcrayfish.framework.api.registry.RegistryEntry;
 import com.mrcrayfish.furniture.refurbished.Constants;
 import com.mrcrayfish.furniture.refurbished.util.Utils;
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.Block;
 
 /**
  * Author: MrCrayfish
@@ -20,7 +23,12 @@ public class ModCreativeTabs
         builder.icon(() -> new ItemStack(ModBlocks.TABLE_OAK.get()));
         builder.title(Component.translatable("itemGroup." + Constants.MOD_ID).withStyle(ChatFormatting.GOLD));
         builder.displayItems((params, output) -> {
-           output.accept(Blocks.OAK_PLANKS);
+            Registration.get(Registries.BLOCK).stream().filter(entry -> entry.getId().getNamespace().equals(Constants.MOD_ID)).forEach(entry -> {
+                output.accept((Block) entry.get());
+            });
+            Registration.get(Registries.ITEM).stream().filter(entry -> entry.getId().getNamespace().equals(Constants.MOD_ID)).forEach(entry -> {
+                output.accept((Item) entry.get());
+            });
         });
     });
 }
