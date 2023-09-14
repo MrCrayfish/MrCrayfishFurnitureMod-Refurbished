1e44b0474fda4d614b0298782c4d43fafd3bad22
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2023-09-14 23:12:38 +0930

    🐛 Fixed duplicate crash when creating creative tab on fabric

--- common/src/main/java/com/mrcrayfish/furniture/refurbished/core/ModCreativeTabs.java
+++ common/src/main/java/com/mrcrayfish/furniture/refurbished/core/ModCreativeTabs.java
@@ -11,7 +11,12 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Consumer;
 
 /**
  * Author: MrCrayfish
@@ -23,11 +28,19 @@ public class ModCreativeTabs
         builder.icon(() -> new ItemStack(ModBlocks.TABLE_OAK.get()));
         builder.title(Component.translatable("itemGroup." + Constants.MOD_ID).withStyle(ChatFormatting.GOLD));
         builder.displayItems((params, output) -> {
+            Set<Item> acceptedItems = new HashSet<>();
+            Consumer<Item> add = item -> {
+                if(item != Items.AIR && !acceptedItems.contains(item)) {
+                    output.accept(item);
+                    acceptedItems.add(item);
+                }
+            };
             Registration.get(Registries.BLOCK).stream().filter(entry -> entry.getId().getNamespace().equals(Constants.MOD_ID)).forEach(entry -> {
-                output.accept((Block) entry.get());
+                Block block = (Block) entry.get();
+                add.accept(block.asItem());
             });
             Registration.get(Registries.ITEM).stream().filter(entry -> entry.getId().getNamespace().equals(Constants.MOD_ID)).forEach(entry -> {
-                output.accept((Item) entry.get());
+                add.accept((Item) entry.get());
             });
         });
     });
