d597a5525c87205d84156b127a267c3afbde81d6
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2023-12-04 22:01:21 +1030

    ✨ Added opne/close sound to kitchen drawer

--- common/src/main/java/com/mrcrayfish/furniture/refurbished/blockentity/KitchenDrawerBlockEntity.java
+++ common/src/main/java/com/mrcrayfish/furniture/refurbished/blockentity/KitchenDrawerBlockEntity.java
@@ -1,9 +1,12 @@
 package com.mrcrayfish.furniture.refurbished.blockentity;
 
 import com.mrcrayfish.furniture.refurbished.core.ModBlockEntities;
+import com.mrcrayfish.furniture.refurbished.core.ModSounds;
 import com.mrcrayfish.furniture.refurbished.util.Utils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -28,5 +31,19 @@ public class KitchenDrawerBlockEntity extends DrawerBlockEntity
     protected Component getDefaultName()
     {
         return Utils.translation("container", "kitchen_drawer");
+    }
+
+    @Override
+    public void onOpen(Level level, BlockPos pos, BlockState state)
+    {
+        level.playSound(null, this.worldPosition, ModSounds.BLOCK_KITCHEN_DRAWER_OPEN.get(), SoundSource.BLOCKS, 1.0F, 0.9F + 0.1F * level.random.nextFloat());
+        super.onOpen(level, pos, state);
+    }
+
+    @Override
+    public void onClose(Level level, BlockPos pos, BlockState state)
+    {
+        level.playSound(null, this.worldPosition, ModSounds.BLOCK_KITCHEN_DRAWER_CLOSE.get(), SoundSource.BLOCKS, 1.0F, 0.9F + 0.1F * level.random.nextFloat());
+        super.onClose(level, pos, state);
     }
 }
