3c9628dea408c0414572aa55f67e23df784cf3a3
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2023-12-12 20:19:03 +1030

    📦 Renamed CT actions

--- common/src/main/java/com/mrcrayfish/furniture/refurbished/compat/crafttweaker/actions/ActionAddItemsToRecycleBinOutput.java
+++ common/src/main/java/com/mrcrayfish/furniture/refurbished/compat/crafttweaker/actions/ActionAddItemsToRecycleBinOutput.java
@@ -20,17 +20,17 @@ import java.util.stream.Collectors;
 /**
  * Author: MrCrayfish
  */
-public class ActionRemoveRecycleBinOutput implements IAction, IRuntimeAction
+public class ActionAddItemsToRecycleBinOutput implements IAction, IRuntimeAction
 {
     private final IRecipeManager<RecycleBinRecyclingRecipe> manager;
     private final ResourceLocation id;
-    private final List<IItemStack> removal;
+    private final List<IItemStack> output;
 
-    public ActionRemoveRecycleBinOutput(IRecipeManager<RecycleBinRecyclingRecipe> manager, ResourceLocation id, List<IItemStack> removal)
+    public ActionAddItemsToRecycleBinOutput(IRecipeManager<RecycleBinRecyclingRecipe> manager, ResourceLocation id, List<IItemStack> output)
     {
         this.manager = manager;
         this.id = id;
-        this.removal = removal;
+        this.output = output;
     }
 
     @Override
@@ -39,7 +39,7 @@ public class ActionRemoveRecycleBinOutput implements IAction, IRuntimeAction
         RecipeList<RecycleBinRecyclingRecipe> list = this.manager.getRecipeList();
         RecycleBinRecyclingRecipe recipe = list.get(this.id);
         List<ItemStack> newOutput = new ArrayList<>(List.of(recipe.getOutputs()));
-        newOutput.removeIf(stack -> this.removal.stream().anyMatch(iStack -> ItemStack.isSameItemSameTags(stack, iStack.getInternal())));
+        this.output.forEach(iStack -> newOutput.add(iStack.getInternal()));
         RecycleBinRecyclingRecipe newRecipe = new RecycleBinRecyclingRecipe(this.id, recipe.getInput(), newOutput.toArray(ItemStack[]::new));
         list.remove(this.id);
         list.add(this.id, newRecipe);
@@ -48,8 +48,8 @@ public class ActionRemoveRecycleBinOutput implements IAction, IRuntimeAction
     @Override
     public String describe()
     {
-        String items = "[" + StringUtils.join(this.removal.stream().map(IItemStack::getCommandString).collect(Collectors.toList()), ", ") + "]";
-        return "Removing the items '%s' from the output of the recycling bin recipe '%s'".formatted(items, this.id);
+        String items = "[" + StringUtils.join(this.output.stream().map(IItemStack::getCommandString).collect(Collectors.toList()), ", ") + "]";
+        return "Adding the items '%s' to the output of the recycling bin '%s'".formatted(items, this.id);
     }
 
     @Override
@@ -66,6 +66,31 @@ public class ActionRemoveRecycleBinOutput implements IAction, IRuntimeAction
             logger.error("No recycling bin recipe exists for the id '{}'", this.id);
             return false;
         }
+
+        RecycleBinRecyclingRecipe recipe = this.manager.getRecipeList().get(this.id);
+        List<ItemStack> outputs = List.of(recipe.getOutputs());
+        for(IItemStack stack : this.output)
+        {
+            if(outputs.stream().anyMatch(stack1 -> ItemStack.isSameItemSameTags(stack1, stack.getInternal())))
+            {
+                logger.error("Cannot add the item '{}' to the output as the item already exists in the output", stack.getCommandString());
+                return false;
+            }
+
+            if(RecycleBinBlockEntity.isInvalidItem(stack.getInternal()))
+            {
+                logger.error("'{}' is not an acceptable item for a recycling bin recipe output", stack.getCommandString());
+                return false;
+            }
+        }
+
+        if(outputs.size() + this.output.size() > RecycleBinRecyclingRecipe.MAX_OUTPUT_COUNT)
+        {
+            String items = "[" + StringUtils.join(this.output.stream().map(IItemStack::getCommandString).collect(Collectors.toList()), ", ") + "]";
+            logger.error("Cannot add the items '{}' as it will exceed the maximum number of allow output items (max: {})", items, RecycleBinRecyclingRecipe.MAX_OUTPUT_COUNT);
+            return false;
+        }
+
         return true;
     }
 }
---
+++ common/src/main/java/com/mrcrayfish/furniture/refurbished/compat/crafttweaker/actions/ActionRemoveItemsFromRecycleBinOutput.java
@@ -0,0 +1,70 @@
+package com.mrcrayfish.furniture.refurbished.compat.crafttweaker.actions;
+
+import com.blamejared.crafttweaker.api.action.base.IAction;
+import com.blamejared.crafttweaker.api.action.base.IRuntimeAction;
+import com.blamejared.crafttweaker.api.item.IItemStack;
+import com.blamejared.crafttweaker.api.recipe.RecipeList;
+import com.blamejared.crafttweaker.api.recipe.manager.base.IRecipeManager;
+import com.mrcrayfish.furniture.refurbished.Constants;
+import com.mrcrayfish.furniture.refurbished.crafting.RecycleBinRecyclingRecipe;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Author: MrCrayfish
+ */
+public class ActionRemoveItemsFromRecycleBinOutput implements IAction, IRuntimeAction
+{
+    private final IRecipeManager<RecycleBinRecyclingRecipe> manager;
+    private final ResourceLocation id;
+    private final List<IItemStack> removal;
+
+    public ActionRemoveItemsFromRecycleBinOutput(IRecipeManager<RecycleBinRecyclingRecipe> manager, ResourceLocation id, List<IItemStack> removal)
+    {
+        this.manager = manager;
+        this.id = id;
+        this.removal = removal;
+    }
+
+    @Override
+    public void apply()
+    {
+        RecipeList<RecycleBinRecyclingRecipe> list = this.manager.getRecipeList();
+        RecycleBinRecyclingRecipe recipe = list.get(this.id);
+        List<ItemStack> newOutput = new ArrayList<>(List.of(recipe.getOutputs()));
+        newOutput.removeIf(stack -> this.removal.stream().anyMatch(iStack -> ItemStack.isSameItemSameTags(stack, iStack.getInternal())));
+        RecycleBinRecyclingRecipe newRecipe = new RecycleBinRecyclingRecipe(this.id, recipe.getInput(), newOutput.toArray(ItemStack[]::new));
+        list.remove(this.id);
+        list.add(this.id, newRecipe);
+    }
+
+    @Override
+    public String describe()
+    {
+        String items = "[" + StringUtils.join(this.removal.stream().map(IItemStack::getCommandString).collect(Collectors.toList()), ", ") + "]";
+        return "Removing the items '%s' from the output of the recycling bin recipe '%s'".formatted(items, this.id);
+    }
+
+    @Override
+    public String systemName()
+    {
+        return Constants.MOD_NAME;
+    }
+
+    @Override
+    public boolean validate(Logger logger)
+    {
+        if(!this.manager.getRecipeList().has(this.id))
+        {
+            logger.error("No recycling bin recipe exists for the id '{}'", this.id);
+            return false;
+        }
+        return true;
+    }
+}
