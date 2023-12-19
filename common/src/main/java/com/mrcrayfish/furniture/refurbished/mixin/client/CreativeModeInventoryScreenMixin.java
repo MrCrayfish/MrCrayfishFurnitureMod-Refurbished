d52bb6e91fc5f2ea91fa2244fa18a2a90d777541
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2023-12-20 02:00:09 +1030

    📦 Merged duplicate mixins into common

--- common/src/main/java/com/mrcrayfish/furniture/refurbished/mixin/client/CreativeModeInventoryScreenMixin.java
+++ common/src/main/java/com/mrcrayfish/furniture/refurbished/mixin/client/CreativeModeInventoryScreenMixin.java
@@ -1,10 +1,14 @@
 package com.mrcrayfish.furniture.refurbished.mixin.client;
 
 import com.mrcrayfish.furniture.refurbished.client.CreativeFilters;
+import com.mrcrayfish.furniture.refurbished.core.ModCreativeTabs;
 import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen;
+import net.minecraft.world.item.CreativeModeTab;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
@@ -13,6 +17,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(CreativeModeInventoryScreen.class)
 public class CreativeModeInventoryScreenMixin
 {
+    @Shadow
+    private static CreativeModeTab selectedTab;
+
     @Inject(method = "mouseScrolled", at = @At(value = "HEAD"), cancellable = true)
     private void refurbishedFurnitureMouseScrollHead(double mouseX, double mouseY, double scroll, CallbackInfoReturnable<Boolean> cir)
     {
@@ -20,5 +27,11 @@ public class CreativeModeInventoryScreenMixin
         {
             cir.setReturnValue(true);
         }
+    }
+
+    @ModifyArg(method = "renderLabels", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;drawString(Lnet/minecraft/client/gui/Font;Lnet/minecraft/network/chat/Component;IIIZ)I"), index = 5)
+    private boolean furnitureRefurbishedRenderLabels(boolean original)
+    {
+        return selectedTab == ModCreativeTabs.MAIN.get();
     }
 }
