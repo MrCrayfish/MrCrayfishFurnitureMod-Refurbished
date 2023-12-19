6bf45e87a5c3fcc6e89a4e663e671d897decdf0e
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2023-12-20 02:48:30 +1030

    📦 Clean up common Forge events

---
+++ forge/src/main/java/com/mrcrayfish/furniture/refurbished/ForgeEvents.java
@@ -0,0 +1,98 @@
+package com.mrcrayfish.furniture.refurbished;
+
+import com.mrcrayfish.furniture.refurbished.blockentity.StorageJarBlockEntity;
+import com.mrcrayfish.furniture.refurbished.blockentity.fluid.FluidContainer;
+import com.mrcrayfish.furniture.refurbished.blockentity.fluid.IFluidContainerBlock;
+import com.mrcrayfish.furniture.refurbished.core.ModItems;
+import com.mrcrayfish.furniture.refurbished.platform.ForgeFluidHelper;
+import com.mrcrayfish.furniture.refurbished.util.Utils;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+import net.minecraftforge.fluids.capability.templates.EmptyFluidHandler;
+import net.minecraftforge.fml.common.Mod;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Author: MrCrayfish
+ */
+@Mod.EventBusSubscriber(modid = Constants.MOD_ID)
+public class ForgeEvents
+{
+    private static final ResourceLocation FLUID_CONTAINER_ID = Utils.resource("fluid_container");
+
+    @SubscribeEvent
+    public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event)
+    {
+        if(event.getItemStack().is(ModItems.WRENCH.get()))
+        {
+            event.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onLeftClickBlock(PlayerInteractEvent.LeftClickBlock event)
+    {
+        Level level = event.getLevel();
+        BlockPos pos = event.getPos();
+        Player player = event.getEntity();
+
+        if(!player.isCreative())
+            return;
+
+        if(player.isCrouching())
+            return;
+
+        if(level.getBlockEntity(pos) instanceof StorageJarBlockEntity storageJar && !storageJar.isEmpty())
+        {
+            if(!level.isClientSide())
+            {
+                storageJar.popItem(player.getDirection().getOpposite());
+            }
+            event.setCanceled(true);
+        }
+    }
+
+    static void onAttachCapability(AttachCapabilitiesEvent<BlockEntity> event)
+    {
+        BlockEntity entity = event.getObject();
+        if(entity instanceof IFluidContainerBlock block)
+        {
+            event.addCapability(FLUID_CONTAINER_ID, new FluidContainerProvider(block));
+        }
+    }
+
+    private static class FluidContainerProvider implements ICapabilityProvider
+    {
+        final LazyOptional<IFluidHandler> holder;
+
+        public FluidContainerProvider(IFluidContainerBlock block)
+        {
+            this.holder = LazyOptional.of(() -> {
+                FluidContainer container = block.getFluidContainer();
+                if(container == null) {
+                    return EmptyFluidHandler.INSTANCE;
+                }
+                return ((ForgeFluidHelper.ForgeFluidContainer) container).getTank();
+            });
+        }
+
+        @Override
+        public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side)
+        {
+            return cap == ForgeCapabilities.FLUID_HANDLER ? this.holder.cast() : LazyOptional.empty();
+        }
+    }
+}
--- forge/src/main/java/com/mrcrayfish/furniture/refurbished/FurnitureMod.java
+++ forge/src/main/java/com/mrcrayfish/furniture/refurbished/FurnitureMod.java
@@ -1,57 +1,32 @@
 package com.mrcrayfish.furniture.refurbished;
 
-import com.mrcrayfish.furniture.refurbished.block.StorageJarBlock;
-import com.mrcrayfish.furniture.refurbished.blockentity.StorageJarBlockEntity;
-import com.mrcrayfish.furniture.refurbished.blockentity.fluid.FluidContainer;
-import com.mrcrayfish.furniture.refurbished.blockentity.fluid.IFluidContainerBlock;
 import com.mrcrayfish.furniture.refurbished.client.ClientBootstrap;
 import com.mrcrayfish.furniture.refurbished.client.ForgeClientEvents;
-import com.mrcrayfish.furniture.refurbished.core.ModItems;
 import com.mrcrayfish.furniture.refurbished.data.FurnitureBlockTagsProvider;
 import com.mrcrayfish.furniture.refurbished.data.FurnitureItemTagsProvider;
 import com.mrcrayfish.furniture.refurbished.data.FurnitureLootTableProvider;
 import com.mrcrayfish.furniture.refurbished.data.FurnitureModelProvider;
 import com.mrcrayfish.furniture.refurbished.data.FurnitureRecipeProvider;
-import com.mrcrayfish.furniture.refurbished.platform.ForgeFluidHelper;
-import com.mrcrayfish.furniture.refurbished.util.Utils;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.ForgeCapabilities;
-import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.data.event.GatherDataEvent;
-import net.minecraftforge.event.AttachCapabilitiesEvent;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fluids.capability.IFluidHandler;
-import net.minecraftforge.fluids.capability.templates.EmptyFluidHandler;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.CompletableFuture;
 
 @Mod(Constants.MOD_ID)
 public class FurnitureMod
 {
-    private static final ResourceLocation FLUID_CONTAINER_ID = Utils.resource("fluid_container");
-
     public FurnitureMod()
     {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -70,9 +45,7 @@ public class FurnitureMod
             MinecraftForge.EVENT_BUS.addListener(ForgeClientEvents::onRenderLevelStage);
             MinecraftForge.EVENT_BUS.addListener(ForgeClientEvents::onDrawHighlight);
         });
-        MinecraftForge.EVENT_BUS.addListener(this::onRightClickBlock);
-        MinecraftForge.EVENT_BUS.addListener(this::onLeftClickBlock);
-        MinecraftForge.EVENT_BUS.addGenericListener(BlockEntity.class, this::onAttachCapability);
+        MinecraftForge.EVENT_BUS.addGenericListener(BlockEntity.class, ForgeEvents::onAttachCapability);
     }
 
     private void onCommonSetup(FMLCommonSetupEvent event)
@@ -96,59 +69,5 @@ public class FurnitureMod
         generator.addProvider(event.includeServer(), new FurnitureLootTableProvider(output));
         generator.addProvider(event.includeServer(), new FurnitureRecipeProvider(output));
         generator.addProvider(event.includeClient(), new FurnitureModelProvider(output, helper));
-    }
-
-    private void onRightClickBlock(PlayerInteractEvent.RightClickBlock event)
-    {
-        if(event.getItemStack().is(ModItems.WRENCH.get()))
-        {
-            event.setCanceled(true);
-        }
-    }
-
-    private void onLeftClickBlock(PlayerInteractEvent.LeftClickBlock event)
-    {
-        Level level = event.getLevel();
-        BlockPos pos = event.getPos();
-        Player player = event.getEntity();
-
-        if(!player.isCreative())
-            return;
-
-        if(player.isCrouching())
-            return;
-
-        if(level.getBlockEntity(pos) instanceof StorageJarBlockEntity storageJar && !storageJar.isEmpty())
-        {
-            if(!level.isClientSide())
-            {
-                storageJar.popItem(player.getDirection().getOpposite());
-            }
-            event.setCanceled(true);
-        }
-    }
-
-    private void onAttachCapability(AttachCapabilitiesEvent<BlockEntity> event)
-    {
-        BlockEntity entity = event.getObject();
-        if(entity instanceof IFluidContainerBlock block)
-        {
-            event.addCapability(FLUID_CONTAINER_ID, new ICapabilityProvider()
-            {
-                final LazyOptional<IFluidHandler> holder = LazyOptional.of(() -> {
-                    FluidContainer container = block.getFluidContainer();
-                    if(container == null) {
-                        return EmptyFluidHandler.INSTANCE;
-                    }
-                    return ((ForgeFluidHelper.ForgeFluidContainer) container).getTank();
-                });
-
-                @Override
-                public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side)
-                {
-                    return cap == ForgeCapabilities.FLUID_HANDLER ? this.holder.cast() : LazyOptional.empty();
-                }
-            });
-        }
     }
 }
