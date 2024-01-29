6a70341e10b56cf73ba3d9930c90185aaf5df6f1
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2024-01-30 00:56:36 +1030

    📦 Added docs to IHomeControlDevice

--- common/src/main/java/com/mrcrayfish/furniture/refurbished/blockentity/IHomeControlDevice.java
+++ common/src/main/java/com/mrcrayfish/furniture/refurbished/blockentity/IHomeControlDevice.java
@@ -8,13 +8,30 @@ import net.minecraft.network.chat.Component;
  */
 public interface IHomeControlDevice
 {
+    /**
+     * @return The block position of the device in the world
+     */
     BlockPos getDevicePos();
 
+    /**
+     * @return True if the device is enabled
+     */
     boolean isDeviceEnabled();
 
+    /**
+     * Toggles the enabled state of the device
+     */
     void toggleDeviceState();
 
+    /**
+     * Sets the enabled state of the device
+     *
+     * @param enabled the new state
+     */
     void setDeviceState(boolean enabled);
 
+    /**
+     * @return The name of the device as a {@link Component}
+     */
     Component getDeviceName();
 }
