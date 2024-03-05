ac06ed73555f5f82a49c6886d28b881e96ad0c16
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2024-03-05 15:12:26 +1030

    🐛 Fixed getting item burn time on neoforge

--- neoforge/src/main/java/com/mrcrayfish/furniture/refurbished/platform/NeoForgeItemHelper.java
+++ neoforge/src/main/java/com/mrcrayfish/furniture/refurbished/platform/NeoForgeItemHelper.java
@@ -3,6 +3,7 @@ package com.mrcrayfish.furniture.refurbished.platform;
 import com.mrcrayfish.furniture.refurbished.platform.services.IItemHelper;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeType;
+import net.neoforged.neoforge.common.CommonHooks;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -13,7 +14,6 @@ public class NeoForgeItemHelper implements IItemHelper
     @Override
     public int getBurnTime(ItemStack stack, @Nullable RecipeType<?> type)
     {
-        return stack.getBurnTime(type);
+        return CommonHooks.getBurnTime(stack, type);
     }
-
 }
