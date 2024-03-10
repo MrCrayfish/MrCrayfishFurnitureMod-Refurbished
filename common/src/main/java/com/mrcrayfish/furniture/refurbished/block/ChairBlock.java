f27cf72e6b65a008648f0c299882b3c2528bd633
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2024-03-10 19:02:14 +1030

    📦 Change chair interaction to be success regardless

--- common/src/main/java/com/mrcrayfish/furniture/refurbished/block/ChairBlock.java
+++ common/src/main/java/com/mrcrayfish/furniture/refurbished/block/ChairBlock.java
@@ -111,7 +111,7 @@ public class ChairBlock extends FurnitureHorizontalBlock implements BlockTagSupp
         {
             return InteractionResult.CONSUME;
         }
-        return InteractionResult.PASS;
+        return InteractionResult.SUCCESS;
     }
 
     @Override
