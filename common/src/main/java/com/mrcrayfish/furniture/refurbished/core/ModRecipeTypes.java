672129a5b2136f67c921fceb44d7901cb4904d9f
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2024-06-30 18:20:35 +0930

    📦 Fix "unknown recipe type" spam in log

--- common/src/main/java/com/mrcrayfish/furniture/refurbished/core/ModRecipeTypes.java
+++ common/src/main/java/com/mrcrayfish/furniture/refurbished/core/ModRecipeTypes.java
@@ -1,11 +1,16 @@
 package com.mrcrayfish.furniture.refurbished.core;
 
+import com.google.common.base.Suppliers;
+import com.google.common.collect.ImmutableList;
 import com.mrcrayfish.framework.api.registry.RegistryContainer;
 import com.mrcrayfish.framework.api.registry.RegistryEntry;
 import com.mrcrayfish.furniture.refurbished.crafting.*;
 import com.mrcrayfish.furniture.refurbished.util.Utils;
 import net.minecraft.world.item.crafting.AbstractCookingRecipe;
 import net.minecraft.world.item.crafting.RecipeType;
+
+import java.util.List;
+import java.util.function.Supplier;
 
 /**
  * Author: MrCrayfish
@@ -22,4 +27,15 @@ public class ModRecipeTypes
     public static final RegistryEntry<RecipeType<MicrowaveHeatingRecipe>> MICROWAVE_HEATING = RegistryEntry.recipeType(Utils.resource("microwave_heating"));
     public static final RegistryEntry<RecipeType<ProcessingRecipe.Item>> FRYING_PAN_COOKING = RegistryEntry.recipeType(Utils.resource("frying_pan_cooking"));
     public static final RegistryEntry<RecipeType<OvenBakingRecipe>> OVEN_BAKING = RegistryEntry.recipeType(Utils.resource("oven_baking"));
+
+    // A list of recipe types that should be ignored when ClientRecipeBook#getCategory is called
+    // This just prevents warning of unknown recipe since they don't use the recipe book system
+    public static final Supplier<List<RecipeType<?>>> IGNORED_RECIPE_TYPES = Suppliers.memoize(() -> ImmutableList.of(
+        WORKBENCH_CONSTRUCTING.get(),
+        GRILL_COOKING.get(),
+        TOASTER_HEATING.get(),
+        CUTTING_BOARD_SLICING.get(),
+        CUTTING_BOARD_COMBINING.get(),
+        FRYING_PAN_COOKING.get()
+    ));
 }
---
+++ common/src/main/java/com/mrcrayfish/furniture/refurbished/mixin/client/ClientRecipeBookMixin.java
@@ -0,0 +1,28 @@
+package com.mrcrayfish.furniture.refurbished.mixin.client;
+
+import com.mrcrayfish.furniture.refurbished.core.ModRecipeTypes;
+import net.minecraft.client.ClientRecipeBook;
+import net.minecraft.client.RecipeBookCategories;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * Author: MrCrayfish
+ */
+@Mixin(ClientRecipeBook.class)
+public class ClientRecipeBookMixin
+{
+    @Inject(method = "getCategory", at = @At(value = "HEAD"), cancellable = true)
+    private static void refurbishedFurniture$GetCategoryHead(Recipe<?> recipe, CallbackInfoReturnable<RecipeBookCategories> cir)
+    {
+        RecipeType<?> type = recipe.getType();
+        if(ModRecipeTypes.IGNORED_RECIPE_TYPES.get().contains(type))
+        {
+            cir.setReturnValue(RecipeBookCategories.UNKNOWN);
+        }
+    }
+}
