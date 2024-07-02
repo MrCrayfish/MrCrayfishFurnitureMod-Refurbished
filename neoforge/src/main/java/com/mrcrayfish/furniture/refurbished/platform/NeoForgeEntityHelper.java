f27ff9d0a9f12ab89eb3dca04753edbc06b662b1
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2024-07-03 03:18:49 +0930

    📦 Update neoforge fake player check

--- neoforge/src/main/java/com/mrcrayfish/furniture/refurbished/platform/NeoForgeEntityHelper.java
+++ neoforge/src/main/java/com/mrcrayfish/furniture/refurbished/platform/NeoForgeEntityHelper.java
@@ -3,6 +3,7 @@ package com.mrcrayfish.furniture.refurbished.platform;
 import com.mrcrayfish.furniture.refurbished.platform.services.IEntityHelper;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.common.util.FakePlayer;
 
 /**
  * Author: MrCrayfish
@@ -12,8 +13,7 @@ public class NeoForgeEntityHelper implements IEntityHelper
     @Override
     public boolean isFakePlayer(Player player)
     {
-        // TODO figure out what to do about fake player
-        return false;
+        return player instanceof FakePlayer;
     }
 
     @Override
