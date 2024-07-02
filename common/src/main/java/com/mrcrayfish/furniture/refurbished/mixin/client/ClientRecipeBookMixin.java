f57c0f805b0e9c453a6ae9fc1ce95f2a12927573
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2024-07-03 02:05:38 +0930

    📦 Fix "unknown recipe type" spam in log

--- common/src/main/java/com/mrcrayfish/furniture/refurbished/mixin/client/ClientRecipeBookMixin.java
+++ common/src/main/java/com/mrcrayfish/furniture/refurbished/mixin/client/ClientRecipeBookMixin.java
@@ -4,6 +4,7 @@ import com.mrcrayfish.furniture.refurbished.core.ModRecipeTypes;
 import net.minecraft.client.ClientRecipeBook;
 import net.minecraft.client.RecipeBookCategories;
 import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeHolder;
 import net.minecraft.world.item.crafting.RecipeType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,9 +18,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class ClientRecipeBookMixin
 {
     @Inject(method = "getCategory", at = @At(value = "HEAD"), cancellable = true)
-    private static void refurbishedFurniture$GetCategoryHead(Recipe<?> recipe, CallbackInfoReturnable<RecipeBookCategories> cir)
+    private static void refurbishedFurniture$GetCategoryHead(RecipeHolder<?> holder, CallbackInfoReturnable<RecipeBookCategories> cir)
     {
-        RecipeType<?> type = recipe.getType();
+        RecipeType<?> type = holder.value().getType();
         if(ModRecipeTypes.IGNORED_RECIPE_TYPES.get().contains(type))
         {
             cir.setReturnValue(RecipeBookCategories.UNKNOWN);
