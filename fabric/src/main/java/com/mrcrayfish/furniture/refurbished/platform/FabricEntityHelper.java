342dde2fde9a1bc3dbab8a4b0d17e489e1128390
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2024-08-05 16:01:17 +0930

    🐛 Fix client code causing crash for dedicated server

--- fabric/src/main/java/com/mrcrayfish/furniture/refurbished/platform/FabricEntityHelper.java
+++ fabric/src/main/java/com/mrcrayfish/furniture/refurbished/platform/FabricEntityHelper.java
@@ -2,6 +2,8 @@ package com.mrcrayfish.furniture.refurbished.platform;
 
 import com.mrcrayfish.furniture.refurbished.platform.services.IEntityHelper;
 import net.fabricmc.fabric.api.entity.FakePlayer;
+import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
+import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 
@@ -20,5 +22,11 @@ public class FabricEntityHelper implements IEntityHelper
     public void spawnFoodParticles(Player player, ItemStack stack)
     {
         player.spawnItemParticles(stack, 10);
+    }
+
+    @Override
+    public SimpleParticleType createSimpleParticleType(boolean ignoreLimit)
+    {
+        return FabricParticleTypes.simple(ignoreLimit);
     }
 }
