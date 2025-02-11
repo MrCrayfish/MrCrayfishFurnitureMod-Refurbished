eeac093b17622b4b7b8867ab482a8355cbfbdcb8
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-02-11 22:13:31 +1030

    ✨ Support more common tags

--- fabric/src/main/java/com/mrcrayfish/furniture/refurbished/platform/FabricTagHelper.java
+++ fabric/src/main/java/com/mrcrayfish/furniture/refurbished/platform/FabricTagHelper.java
@@ -1,6 +1,6 @@
 package com.mrcrayfish.furniture.refurbished.platform;
 
-import com.mrcrayfish.furniture.refurbished.compat.CompatibilityTags;
+import com.mrcrayfish.furniture.refurbished.core.ModTags;
 import com.mrcrayfish.furniture.refurbished.platform.services.ITagHelper;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
@@ -13,6 +13,6 @@ public class FabricTagHelper implements ITagHelper
     @Override
     public TagKey<Item> getToolKnivesTag()
     {
-        return CompatibilityTags.Items.FABRIC_TOOLS_KNIVES;
+        return ModTags.Items.TOOLS_KNIVES;
     }
 }
