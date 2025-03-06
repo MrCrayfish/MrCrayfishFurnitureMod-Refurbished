26c896bde812c9309e0302020b1363a1aabec555
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-03-06 14:24:37 +1030

    🐛 Fix items not saving after clearing content

--- common/src/main/java/com/mrcrayfish/furniture/refurbished/blockentity/CuttingBoardBlockEntity.java
+++ common/src/main/java/com/mrcrayfish/furniture/refurbished/blockentity/CuttingBoardBlockEntity.java
@@ -375,6 +375,7 @@ public class CuttingBoardBlockEntity extends BasicLootBlockEntity
         List<ItemStack> remainingItems = this.getCraftingRemainingItems();
 
         this.clearContent();
+        this.setChanged();
         this.spawnSliceParticles(stack);
         this.spawnMagicParticles();
 
