9d5a2095e6871ad06ade57a6d741ff99f2d7135e
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-06-02 13:44:18 +0930

    🐛 Fix crash when breaking television

--- forge/src/main/java/com/mrcrayfish/furniture/refurbished/client/ForgeRenderType.java
+++ forge/src/main/java/com/mrcrayfish/furniture/refurbished/client/ForgeRenderType.java
@@ -16,7 +16,7 @@ import java.util.function.Function;
 public class ForgeRenderType extends RenderType
 {
     private static final Function<ResourceLocation, RenderType> TELEVISION_SCREEN = Util.memoize((id) -> {
-        return RenderType.create(Constants.MOD_ID + "_television_screen", DefaultVertexFormat.BLOCK, VertexFormat.Mode.QUADS, 0x200000, true, false, RenderType.CompositeState.builder()
+        return RenderType.create(Constants.MOD_ID + "_television_screen", DefaultVertexFormat.BLOCK, VertexFormat.Mode.QUADS, 0x200000, false, false, RenderType.CompositeState.builder()
                 .setLightmapState(LIGHTMAP)
                 .setShaderState(RENDERTYPE_SOLID_SHADER)
                 .setTextureState(new RenderStateShard.TextureStateShard(id, false, false))
