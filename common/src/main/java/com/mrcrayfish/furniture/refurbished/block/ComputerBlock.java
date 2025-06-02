f31c9e6cb1406b49274f86ef1a1eebc5594292a1
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-06-02 12:17:36 +0930

    🐛 Fix error thrown when open computer in spectator

--- common/src/main/java/com/mrcrayfish/furniture/refurbished/block/ComputerBlock.java
+++ common/src/main/java/com/mrcrayfish/furniture/refurbished/block/ComputerBlock.java
@@ -4,7 +4,6 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.mrcrayfish.framework.api.FrameworkAPI;
 import com.mrcrayfish.furniture.refurbished.blockentity.ComputerBlockEntity;
-import com.mrcrayfish.furniture.refurbished.core.ModBlockEntities;
 import com.mrcrayfish.furniture.refurbished.data.tag.BlockTagSupplier;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -13,18 +12,18 @@ import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityTicker;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.Nullable;
 
-import javax.annotation.Nullable;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -56,7 +55,7 @@ public class ComputerBlock extends FurnitureHorizontalEntityBlock implements Blo
         {
             if(!level.isClientSide() && level.getBlockEntity(pos) instanceof ComputerBlockEntity computer && computer.isNodePowered() && !computer.isBeingUsed())
             {
-                if(FrameworkAPI.openMenuWithData((ServerPlayer) player, computer, buf -> buf.writeBlockPos(pos)).isPresent())
+                if(FrameworkAPI.openMenuWithData((ServerPlayer) player, computer.createMenuProvider(), buf -> buf.writeBlockPos(pos)).isPresent())
                 {
                     computer.syncStateToPlayer(player);
                 }
@@ -65,6 +64,14 @@ public class ComputerBlock extends FurnitureHorizontalEntityBlock implements Blo
             return InteractionResult.SUCCESS;
         }
         return InteractionResult.PASS;
+    }
+
+    @Override
+    @Nullable
+    public MenuProvider getMenuProvider(BlockState state, Level level, BlockPos pos)
+    {
+        // Basically disables spectator from opening computer
+        return null;
     }
 
     @Override
