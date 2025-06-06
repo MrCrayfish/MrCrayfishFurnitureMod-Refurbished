9fe0206cbf5f4665405043c3a4d7ff1cada8a6a5
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-06-06 16:59:48 +0930

    🐛 Fix potential memory leak with render types

--- fabric/src/main/java/com/mrcrayfish/furniture/refurbished/client/FabricRenderType.java
+++ fabric/src/main/java/com/mrcrayfish/furniture/refurbished/client/FabricRenderType.java
@@ -1,14 +1,18 @@
 package com.mrcrayfish.furniture.refurbished.client;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mrcrayfish.furniture.refurbished.Constants;
 import com.mrcrayfish.furniture.refurbished.core.ModRenderPipelines;
+import com.mrcrayfish.furniture.refurbished.image.TextureCache;
 import net.minecraft.Util;
 import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.texture.AbstractTexture;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.TriState;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 /**
@@ -23,8 +27,41 @@ public class FabricRenderType
                 .createCompositeState(true));
     });
 
+    private static final Function<ResourceLocation, RenderType> PALETTE_IMAGE = id -> {
+        return RenderType.create(Constants.MOD_ID + "_palette_image", 0x200000, false, false, RenderPipelines.CUTOUT, RenderType.CompositeState.builder()
+                .setLightmapState(RenderType.LIGHTMAP)
+                .setTextureState(new DoorMatTextureStateShard(id))
+                .createCompositeState(true));
+    };
+
     public static RenderType televisionScreen(ResourceLocation id)
     {
         return TELEVISION_SCREEN.apply(id);
+    }
+
+    public static RenderType createPaletteImage(ResourceLocation id)
+    {
+        return PALETTE_IMAGE.apply(id);
+    }
+
+    private static class DoorMatTextureStateShard extends RenderStateShard.EmptyTextureStateShard
+    {
+        private final Optional<ResourceLocation> texture;
+
+        public DoorMatTextureStateShard(ResourceLocation id)
+        {
+            super(() -> {
+                AbstractTexture texture = TextureCache.get().getTexture(id);
+                texture.setFilter(TriState.FALSE, false);
+                RenderSystem.setShaderTexture(0, texture.getTexture());
+            }, () -> {});
+            this.texture = Optional.of(id);
+        }
+
+        @Override
+        protected Optional<ResourceLocation> cutoutTexture()
+        {
+            return this.texture;
+        }
     }
 }
